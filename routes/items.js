b0a3200c9698f3cac7c5d22833d29f5e5af36444
Silenio Quarti <silenio_quarti@ca.ibm.com>
2016-11-11 09:10:19 -0600

    test

--- routes/items.js
+++ routes/items.js
@@ -11,7 +11,6 @@ var USE_FASTCACHE = false;
  */
 exports.loadTest = function(req, res) {
     res.json({"success": 0, "fail": 0, "time": 0});
-/*
     USE_FASTCACHE = true;
     var testCount = req.query.count;
     testCount = testCount ? parseInt(testCount) : 100;
@@ -40,7 +39,6 @@ exports.loadTest = function(req, res) {
             path: "/items/" + (i % 2 ? itemId1 : itemId2)
 	}, callback);
     }
-*/
 };
 
 
